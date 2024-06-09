cdaa5c163b66adca89ddd83534b9ce7e696f3708
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-06-08 21:21:55 -0300

    feat: tailwind project config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18D760',
+        'purple-main': '#2F46B9',
+        'green-main': '#1ED760'
+      },
+      backgroundColor: theme => ({
+        'spotify-theme': "url('img\bursts.svg')",
+        'spotify-theme-mobile': "url('img\bursts-mobile.svg')"
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%'
+
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize: {
+        '9xl': '9rem'
+      }
+
+    },
   },
   plugins: [],
 }
